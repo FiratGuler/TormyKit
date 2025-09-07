722e7b67b32bb2c2ed155140beace7cae2912fd1
Fırat Güler <firattgulerrr@gmail.com>
2025-09-07 17:16:50 +0300

    first

---
+++ Sources/TormyUIKit/Constants/TormyLayout.swift
@@ -0,0 +1,29 @@
+//
+//  AppConstants.swift
+//  Wordy
+//
+//  Created by Fırat Güler on 4.09.2025.
+//
+
+import UIKit
+
+struct TormyLayout {
+    
+    private init() {}
+    
+    // Corner Radius
+    static let cornerRadius: CGFloat = 8.0
+    
+    // Margins
+    static let margin: CGFloat = 8.0
+    static let margin2x: CGFloat = 16.0
+    static let margin3x: CGFloat = 24.0
+    static let margin4x: CGFloat = 32.0
+    static let margin5x: CGFloat = 40.0
+    
+    // Alert
+    static let toastHeight: CGFloat = 60.0
+    static let toastTopPadding: CGFloat = 16.0
+}
+
+
