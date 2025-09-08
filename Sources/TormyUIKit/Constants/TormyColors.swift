1e77f64c40133ff916bef8980fd45b2a968881a7
FiratGuler <firattgulerrr@gmail.com>
2025-09-08 09:25:50 +0300

    TormyColors neutral add

--- Sources/TormyUIKit/Constants/TormyColors.swift
+++ Sources/TormyUIKit/Constants/TormyColors.swift
@@ -8,11 +8,6 @@
 import UIKit
 
 public struct ColorConfigure {
-    public var backgroundColor: String?
-    public var containerColor: String?
-    public var borderColor: String?
-    public var textFieldColor: String?
-    
     public var primary900: String?
     public var primary800: String?
     public var primary700: String?
@@ -23,6 +18,17 @@ public struct ColorConfigure {
     public var primary200: String?
     public var primary100: String?
     public var primary50: String?
+    
+    public var neutral900: String?
+    public var neutral800: String?
+    public var neutral700: String?
+    public var neutral600: String?
+    public var neutral500: String?
+    public var neutral400: String?
+    public var neutral300: String?
+    public var neutral200: String?
+    public var neutral100: String?
+    public var neutral50: String?
     
     public var greyScale900: String?
     public var greyScale800: String?
@@ -42,11 +48,7 @@ public struct ColorConfigure {
     public var success: String?
     public var warning: String?
     
-    public init(background: String? = nil, containerColor: String? = nil, borderColor: String? = nil, textFieldColor: String? = nil, primary900: String? = nil, primary800: String? = nil, primary700: String? = nil, primary600: String? = nil, primary500: String? = nil, primary400: String? = nil, primary300: String? = nil, primary200: String? = nil, primary100: String? = nil, primary50: String? = nil, greyScale900: String? = nil, greyScale800: String? = nil, greyScale700: String? = nil, greyScale600: String? = nil, greyScale500: String? = nil, greyScale400: String? = nil, greyScale300: String? = nil, greyScale200: String? = nil, greyScale100: String? = nil, greyScale50: String? = nil, black: String? = nil, white: String? = nil, error: String? = nil, success: String? = nil, warning: String? = nil) {
-        self.backgroundColor = background
-        self.containerColor = containerColor
-        self.borderColor = borderColor
-        self.textFieldColor = textFieldColor
+    public init(primary900: String? = nil, primary800: String? = nil, primary700: String? = nil, primary600: String? = nil, primary500: String? = nil, primary400: String? = nil, primary300: String? = nil, primary200: String? = nil, primary100: String? = nil, primary50: String? = nil, neutral900: String? = nil, neutral800: String? = nil, neutral700: String? = nil, neutral600: String? = nil, neutral500: String? = nil, neutral400: String? = nil, neutral300: String? = nil, neutral200: String? = nil, neutral100: String? = nil, neutral50: String? = nil, greyScale900: String? = nil, greyScale800: String? = nil, greyScale700: String? = nil, greyScale600: String? = nil, greyScale500: String? = nil, greyScale400: String? = nil, greyScale300: String? = nil, greyScale200: String? = nil, greyScale100: String? = nil, greyScale50: String? = nil, black: String? = nil, white: String? = nil, error: String? = nil, success: String? = nil, warning: String? = nil) {
         self.primary900 = primary900
         self.primary800 = primary800
         self.primary700 = primary700
@@ -57,6 +59,16 @@ public struct ColorConfigure {
         self.primary200 = primary200
         self.primary100 = primary100
         self.primary50 = primary50
+        self.neutral900 = neutral900
+        self.neutral800 = neutral800
+        self.neutral700 = neutral700
+        self.neutral600 = neutral600
+        self.neutral500 = neutral500
+        self.neutral400 = neutral400
+        self.neutral300 = neutral300
+        self.neutral200 = neutral200
+        self.neutral100 = neutral100
+        self.neutral50 = neutral50
         self.greyScale900 = greyScale900
         self.greyScale800 = greyScale800
         self.greyScale700 = greyScale700
@@ -83,11 +95,6 @@ public struct TormyColors {
     private static let defaultColor = "#0ef078"
     
     // MARK: - Cached UIColor
-    private static var _backgroundColor: UIColor = UIColor(hex: defaultColor)
-    private static var _containerColor: UIColor = UIColor(hex: defaultColor)
-    private static var _borderColor: UIColor = UIColor(hex: defaultColor)
-    private static var _textFieldColor: UIColor = UIColor(hex: defaultColor)
-    
     private static var _primary900: UIColor = UIColor(hex: defaultColor)
     private static var _primary800: UIColor = UIColor(hex: defaultColor)
     private static var _primary700: UIColor = UIColor(hex: defaultColor)
@@ -98,6 +105,17 @@ public struct TormyColors {
     private static var _primary200: UIColor = UIColor(hex: defaultColor)
     private static var _primary100: UIColor = UIColor(hex: defaultColor)
     private static var _primary50: UIColor = UIColor(hex: defaultColor)
+    
+    private static var _neutral900: UIColor = UIColor(hex: defaultColor)
+    private static var _neutral800: UIColor = UIColor(hex: defaultColor)
+    private static var _neutral700: UIColor = UIColor(hex: defaultColor)
+    private static var _neutral600: UIColor = UIColor(hex: defaultColor)
+    private static var _neutral500: UIColor = UIColor(hex: defaultColor)
+    private static var _neutral400: UIColor = UIColor(hex: defaultColor)
+    private static var _neutral300: UIColor = UIColor(hex: defaultColor)
+    private static var _neutral200: UIColor = UIColor(hex: defaultColor)
+    private static var _neutral100: UIColor = UIColor(hex: defaultColor)
+    private static var _neutral50: UIColor = UIColor(hex: defaultColor)
     
     private static var _greyScale900: UIColor = UIColor(hex: defaultColor)
     private static var _greyScale800: UIColor = UIColor(hex: defaultColor)
@@ -122,13 +140,7 @@ public struct TormyColors {
     // MARK: - Configure
     static func configure(theme: ColorConfigure) {
         self.theme = theme
-        
-        // Base
-        _backgroundColor = UIColor(hex: theme.backgroundColor ?? defaultColor)
-        _containerColor = UIColor(hex: theme.containerColor ?? defaultColor)
-        _borderColor = UIColor(hex: theme.borderColor ?? defaultColor)
-        _textFieldColor = UIColor(hex: theme.textFieldColor ?? defaultColor)
-        
+                
         // Primary
         _primary900 = UIColor(hex: theme.primary900 ?? defaultColor)
         _primary800 = UIColor(hex: theme.primary800 ?? defaultColor)
@@ -140,6 +152,18 @@ public struct TormyColors {
         _primary200 = UIColor(hex: theme.primary200 ?? defaultColor)
         _primary100 = UIColor(hex: theme.primary100 ?? defaultColor)
         _primary50 = UIColor(hex: theme.primary50 ?? defaultColor)
+        
+        //Neutral
+        _neutral900 = UIColor(hex: theme.neutral900 ?? defaultColor)
+        _neutral800 = UIColor(hex: theme.neutral800 ?? defaultColor)
+        _neutral700 = UIColor(hex: theme.neutral700 ?? defaultColor)
+        _neutral600 = UIColor(hex: theme.neutral600 ?? defaultColor)
+        _neutral500 = UIColor(hex: theme.neutral500 ?? defaultColor)
+        _neutral400 = UIColor(hex: theme.neutral400 ?? defaultColor)
+        _neutral300 = UIColor(hex: theme.neutral300 ?? defaultColor)
+        _neutral200 = UIColor(hex: theme.neutral200 ?? defaultColor)
+        _neutral100 = UIColor(hex: theme.neutral100 ?? defaultColor)
+        _neutral50 = UIColor(hex: theme.neutral50 ?? defaultColor)
         
         // GreyScale
         _greyScale900 = UIColor(hex: theme.greyScale900 ?? defaultColor)
@@ -162,13 +186,7 @@ public struct TormyColors {
         _success = UIColor(hex: theme.success ?? defaultColor)
         _warning = UIColor(hex: theme.warning ?? defaultColor)
     }
-    
-    
-    public static var backgroundColor: UIColor { _backgroundColor }
-    public static var containerColor: UIColor { _containerColor }
-    public static var borderColor: UIColor { _borderColor }
-    public static var textFieldColor: UIColor { _textFieldColor }
-    
+        
     public static var primary900: UIColor { _primary900 }
     public static var primary800: UIColor { _primary800 }
     public static var primary700: UIColor { _primary700 }
@@ -180,6 +198,17 @@ public struct TormyColors {
     public static var primary100: UIColor { _primary100 }
     public static var primary50: UIColor { _primary50 }
     
+    public static var neutral900: UIColor { _neutral900 }
+    public static var neutral800: UIColor { _neutral800 }
+    public static var neutral700: UIColor { _neutral700 }
+    public static var neutral600: UIColor { _neutral600 }
+    public static var neutral500: UIColor { _neutral500 }
+    public static var neutral400: UIColor { _neutral400 }
+    public static var neutral300: UIColor { _neutral300 }
+    public static var neutral200: UIColor { _neutral200 }
+    public static var neutral100: UIColor { _neutral100 }
+    public static var neutral50: UIColor { _neutral50 }
+
     public static var greyScale900: UIColor { _greyScale900 }
     public static var greyScale800: UIColor { _greyScale800 }
     public static var greyScale700: UIColor { _greyScale700 }
