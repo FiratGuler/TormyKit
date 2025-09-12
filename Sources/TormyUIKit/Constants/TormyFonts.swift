8261bcd883c19e78550e06ef828cf53c82ac22c4
FiratGuler <firattgulerrr@gmail.com>
2025-09-12 22:52:22 +0300

    Fonts Family update

--- Sources/TormyUIKit/Constants/TormyFonts.swift
+++ Sources/TormyUIKit/Constants/TormyFonts.swift
@@ -103,60 +103,80 @@ public struct TormyFonts {
     }
     
     // H1
+    public static var h1_black: UIFont { font(type: .black, size: .h1) }
+    public static var h1_ExtraBold: UIFont { font(type: .extraBold, size: .h1) }
     public static var h1_bold: UIFont { font(type: .bold, size: .h1) }
     public static var h1_semiBold: UIFont { font(type: .semiBold, size: .h1) }
     public static var h1_medium: UIFont { font(type: .medium, size: .h1) }
     public static var h1_regular: UIFont { font(type: .regular, size: .h1) }
     
     // H2
+    public static var h2_black: UIFont { font(type: .black, size: .h2) }
+    public static var h2_ExtraBold: UIFont { font(type: .extraBold, size: .h2) }
     public static var h2_bold: UIFont { font(type: .bold, size: .h2) }
     public static var h2_semiBold: UIFont { font(type: .semiBold, size: .h2) }
     public static var h2_medium: UIFont { font(type: .medium, size: .h2) }
     public static var h2_regular: UIFont { font(type: .regular, size: .h2) }
     
     // H3
+    public static var h3_black: UIFont { font(type: .black, size: .h3) }
+    public static var h3_ExtraBold: UIFont { font(type: .extraBold, size: .h3) }
     public static var h3_bold: UIFont { font(type: .bold, size: .h3) }
     public static var h3_semiBold: UIFont { font(type: .semiBold, size: .h3) }
     public static var h3_medium: UIFont { font(type: .medium, size: .h3) }
     public static var h3_regular: UIFont { font(type: .regular, size: .h3) }
     
     // H4
+    public static var h4_black: UIFont { font(type: .black, size: .h4) }
+    public static var h4_ExtraBold: UIFont { font(type: .extraBold, size: .h4) }
     public static var h4_bold: UIFont { font(type: .bold, size: .h4) }
     public static var h4_semiBold: UIFont { font(type: .semiBold, size: .h4) }
     public static var h4_medium: UIFont { font(type: .medium, size: .h4) }
     public static var h4_regular: UIFont { font(type: .regular, size: .h4) }
     
     // H5
+    public static var h5_black: UIFont { font(type: .black, size: .h5) }
+    public static var h5_ExtraBold: UIFont { font(type: .extraBold, size: .h5) }
     public static var h5_bold: UIFont { font(type: .bold, size: .h5) }
     public static var h5_semiBold: UIFont { font(type: .semiBold, size: .h5) }
     public static var h5_medium: UIFont { font(type: .medium, size: .h5) }
     public static var h5_regular: UIFont { font(type: .regular, size: .h5) }
     
     // Xlarge
+    public static var xLarge_black: UIFont { font(type: .black, size: .xLarge) }
+    public static var xLarge_ExtraBold: UIFont { font(type: .extraBold, size: .xLarge) }
     public static var xLarge_bold: UIFont { font(type: .bold, size: .xLarge) }
     public static var xLarge_semiBold: UIFont { font(type: .semiBold, size: .xLarge) }
     public static var xLarge_medium: UIFont { font(type: .medium, size: .xLarge) }
     public static var xLarge_regular: UIFont { font(type: .regular, size: .xLarge) }
     
     // Large
+    public static var large_black: UIFont { font(type: .black, size: .large) }
+    public static var large_ExtraBold: UIFont { font(type: .extraBold, size: .large) }
     public static var large_bold: UIFont { font(type: .bold, size: .large) }
     public static var large_semiBold: UIFont { font(type: .semiBold, size: .large) }
     public static var large_medium: UIFont { font(type: .medium, size: .large) }
     public static var large_regular: UIFont { font(type: .regular, size: .large) }
     
     // Medium
+    public static var medium_black: UIFont { font(type: .black, size: .medium) }
+    public static var medium_ExtraBold: UIFont { font(type: .extraBold, size: .medium) }
     public static var medium_bold: UIFont { font(type: .bold, size: .medium) }
     public static var medium_semiBold: UIFont { font(type: .semiBold, size: .medium) }
     public static var medium_medium: UIFont { font(type: .medium, size: .medium) }
     public static var medium_regular: UIFont { font(type: .regular, size: .medium) }
     
     // Small
+    public static var small_black: UIFont { font(type: .black, size: .small) }
+    public static var small_ExtraBold: UIFont { font(type: .extraBold, size: .small) }
     public static var small_bold: UIFont { font(type: .bold, size: .small) }
     public static var small_semiBold: UIFont { font(type: .semiBold, size: .small) }
     public static var small_medium: UIFont { font(type: .medium, size: .small) }
     public static var small_regular: UIFont { font(type: .regular, size: .small) }
     
     // XSmall
+    public static var xSmall_black: UIFont { font(type: .black, size: .xSmall) }
+    public static var xSmall_ExtraBold: UIFont { font(type: .extraBold, size: .xSmall) }
     public static var xSmall_bold: UIFont { font(type: .bold, size: .xSmall) }
     public static var xSmall_semiBold: UIFont { font(type: .semiBold, size: .xSmall) }
     public static var xSmall_medium: UIFont { font(type: .medium, size: .xSmall) }
