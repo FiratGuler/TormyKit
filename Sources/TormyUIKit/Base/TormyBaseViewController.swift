1a08cb26c52bcdf8a8f2f87fae9a65d6b64add69
FiratGuler <firattgulerrr@gmail.com>
2025-09-19 23:00:57 +0300

    NavigationBar title customize add

--- Sources/TormyUIKit/Base/TormyBaseViewController.swift
+++ Sources/TormyUIKit/Base/TormyBaseViewController.swift
@@ -10,18 +10,33 @@ import SnapKit
 
 open class TormyBaseViewController: UIViewController {
     
-    open var appBackground: UIColor {
-        return TormyColors.neutral900
-    }
-
+    open var appBackground: UIColor { TormyColors.neutral900 }
+    
+    // Navigation title konfigürasyonu
+    open var navigationTitleText: String { "" }
+    open var navigationTitleColor: UIColor { TormyColors.primary900 }
+    open var navigationTitleFont: UIFont { TormyFonts.h4_bold }
+    
     override open func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = appBackground 
+        view.backgroundColor = appBackground
+        configureNavigationTitle()
     }
     
+    private func configureNavigationTitle() {
+        let attributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: navigationTitleColor,
+            .font: navigationTitleFont
+        ]
+        navigationController?.navigationBar.titleTextAttributes = attributes
+        navigationItem.title = navigationTitleText
+    }
+    
+    // MARK: - Alert
+    // MARK: Toast
     public func showToast(message: String, status: ToastView.ToastStatus, duration: TimeInterval = 2.3) {
         let toastView = ToastView(message: message, status: status)
-
+        
         view.addSubview(toastView)
         toastView.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(TormyLayout.toastTopPadding)
@@ -29,56 +44,29 @@ open class TormyBaseViewController: UIViewController {
             make.height.equalTo(TormyLayout.toastHeight)
             make.left.right.equalToSuperview().inset(TormyLayout.toastTopPadding)
         }
-
+        
         toastView.fadeInOut(visibleDuration: duration) {
             toastView.removeFromSuperview()
         }
     }
     
+    // MARK: PopUp
     public func showPopUp(title: String,
-                              message: String,
-                              positiveButton: String,
-                              positiveAction: (() -> Void)? = nil,
-                              negativeButton: String? = nil,
-                              negativeAction: (() -> Void)? = nil) {
-
-            let popup = PopUpView(title: title,
-                                  message: message,
-                                  positiveButton: positiveButton,
-                                  positiveAction: positiveAction,
-                                  negativeButton: negativeButton,
-                                  negativeAction: negativeAction)
-
-            let popupVC = PopUpViewController(popup: popup)
-            present(popupVC, animated: true)
-        }
-//    
-//    public func showPopUp(title: String, message: String, positiveButton: String, positiveAction: (() -> Void)? = nil) {
-//
-//
-//        
-//                let backgroundView = UIView()
-//        backgroundView.backgroundColor = UIColor.black.withAlphaComponent(0.5)
-//        backgroundView.alpha = 0.0
-//        view.addSubview(backgroundView)
-//        backgroundView.snp.makeConstraints { make in
-//            make.edges.equalToSuperview()
-//        }
-//        
-//        let popUp = PopUpView(title: title, message: message, positiveButton: positiveButton, positiveAction: positiveAction)
-//        
-//        view.addSubview(popUp)
-//        popUp.snp.makeConstraints { make in
-//            make.centerY.equalToSuperview()
-//            make.left.right.equalToSuperview().inset(TormyLayout.margin3x)
-//            make.height.equalToSuperview().dividedBy(3)
-//        }
-//        
-//        UIView.animate(withDuration: 0.25) {
-//            backgroundView.alpha = 1.0
-//        }
-//
-//    }
-//    
-
+                          message: String,
+                          positiveButton: String,
+                          positiveAction: (() -> Void)? = nil,
+                          negativeButton: String? = nil,
+                          negativeAction: (() -> Void)? = nil) {
+        
+        let popup = PopUpView(title: title,
+                              message: message,
+                              positiveButton: positiveButton,
+                              positiveAction: positiveAction,
+                              negativeButton: negativeButton,
+                              negativeAction: negativeAction)
+        
+        let popupVC = PopUpViewController(popup: popup)
+        present(popupVC, animated: true)
+    }
+    
 }
